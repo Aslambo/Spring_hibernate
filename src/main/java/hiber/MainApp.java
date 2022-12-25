fe718786a8067a303bdd0fdebbd3c55542346085
Aslambek Sosaev <105995973+Aslambo@users.noreply.github.com>
2022-12-26 02:30:57 +0300

    Initial commit

---
+++ src/main/java/hiber/MainApp.java
@@ -0,0 +1,47 @@
+package hiber;
+
+import hiber.config.AppConfig;
+import hiber.model.Car;
+import hiber.model.User;
+import hiber.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class MainApp {
+    public static void main(String[] args) throws SQLException {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+
+        UserService userService = context.getBean(UserService.class);
+
+        User user1 = new User("User1", "Lastname1", "user1@mail.ru");
+        user1.setUserCar(new Car("bmw", 3));
+        User user2 = new User("User2", "Lastname2", "user2@mail.ru");
+        user2.setUserCar(new Car("mers", 220));
+        User user3 = new User("User3", "Lastname3", "user3@mail.ru");
+        user3.setUserCar(new Car("ford", 750));
+        User user4 = new User("User4", "Lastname4", "user4@mail.ru");
+        user4.setUserCar(new Car("toyota", 35));
+        userService.add(user1);
+        userService.add(user2);
+        userService.add(user3);
+        userService.add(user4);
+        System.out.println("--------------------------------------------------------------------------");
+        System.out.println(userService.getUserByCar("toyota", 35));
+        System.out.println("--------------------------------------------------------------------------");
+
+
+        List<User> users = userService.listUsers();
+        for (User user : users) {
+            System.out.println("Id = " + user.getId());
+            System.out.println("First Name = " + user.getFirstName());
+            System.out.println("Last Name = " + user.getLastName());
+            System.out.println("Email = " + user.getEmail());
+            System.out.println();
+        }
+
+        context.close();
+    }
+}
---
+++ src/main/java/hiber/model/Car.java
@@ -0,0 +1,55 @@
+package hiber.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Column;
+
+@Entity
+@Table(name = "cars")
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column
+    private String model;
+    @Column
+    private int series;
+
+    public Car() {
+    }
+
+    @Autowired
+    public Car(String model, int series) {
+        this.model = model;
+        this.series = series;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+}
